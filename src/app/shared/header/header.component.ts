2051ba0e30179da80ae3e2bb22a1bbd92d6a546b
primadita <pulomas10@hotmail.com>
2025-09-11 13:50:31 +0200

    feat: all animations of landing page

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
