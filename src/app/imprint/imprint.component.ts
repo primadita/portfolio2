d9111b2e38f5d2374bda8848d62121ad077920c5
primadita <pulomas10@hotmail.com>
2025-09-18 14:29:21 +0200

    feat: add impressum

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
