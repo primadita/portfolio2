84d479e1ee3ca62e4bd23dc5a1879a3bc140e59c
primadita <pulomas10@hotmail.com>
2025-09-18 05:20:12 +0200

    feat: add contact page

--- src/app/main-content/contact-page/contact-page.component.ts
+++ src/app/main-content/contact-page/contact-page.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact-page',
-  imports: [],
+  imports: [TranslateModule, CommonModule, FormsModule],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.scss'
 })
 export class ContactPageComponent {
+  contactData = {
+    name: "",
+    email: "",
+    messsage:""
+  }
+  privacyChecked = false;
 
+  onSubmit(ngForm: NgForm){
+    console.log(this.contactData);
+  }
 }
