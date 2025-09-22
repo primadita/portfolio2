905fc6f87b8479ed844279a1c0bc1c8c8565d22c
primadita <pulomas10@hotmail.com>
2025-09-22 14:08:13 +0200

    feat: add language choice to local storage

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TranslateDirective, TranslateModule, TranslatePipe, TranslateService } from '@ngx-translate/core';
 
@@ -10,11 +10,14 @@ import { TranslateDirective, TranslateModule, TranslatePipe, TranslateService } 
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   currentLanguage: string = 'en';
+  savedLang!: string;
+  
 
   constructor(private translate:TranslateService){
-    this.translate.addLangs(['de','en']);
+    // this.translate.addLangs(['de','en']);
+    // this.currentLanguage = this.savedLang ? this.savedLang:'en';
     this.translate.setDefaultLang(this.currentLanguage);
     this.translate.use(this.currentLanguage);
   }
@@ -22,5 +25,11 @@ export class HeaderComponent {
   useLanguage(language: string):void {
     this.currentLanguage = language;
     this.translate.use(language);
+    localStorage.setItem('language',language);
+  }
+  ngOnInit(): void {
+    this.savedLang = localStorage.getItem('language') || 'en';
+    this.currentLanguage = this.savedLang;
+    this.translate.use(this.currentLanguage);
   }
 }
