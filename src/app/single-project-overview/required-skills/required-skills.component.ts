89db55399d585e966925ae737bc6b3c021d7c6a9
primadita <pulomas10@hotmail.com>
2025-09-24 01:07:34 +0200

    doc: add doc to single project overview

--- src/app/single-project-overview/required-skills/required-skills.component.ts
+++ src/app/single-project-overview/required-skills/required-skills.component.ts
@@ -2,6 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Skill } from '../../shared/interfaces/skill';
 
+/**
+ * Component responsible for displaying a programming language 
+ * knowledge that is necessary for the project.
+ * 
+ * Receives a {@link Skill} object as input and 
+ * renders it in the template.
+ */
 @Component({
   selector: 'app-required-skills',
   imports: [CommonModule],
@@ -9,5 +16,13 @@ import { Skill } from '../../shared/interfaces/skill';
   styleUrl: './required-skills.component.scss'
 })
 export class RequiredSkillsComponent {
+  // #region ATTRIBUTES
+  /**
+   * The skill to be displayed, including its icon and related metadata.
+   *
+   * @input
+   * @type {Skill}
+   */
   @Input() icon!: Skill;
+  // #endregion
 }
--- src/app/single-project-overview/single-project-overview.component.ts
+++ src/app/single-project-overview/single-project-overview.component.ts
@@ -5,9 +5,16 @@ import { RequiredSkillsComponent } from './required-skills/required-skills.compo
 import { ActivatedRoute } from '@angular/router';
 import { ProjectDescriptionListService } from '../services/project-description-list.service';
 import { Project } from '../shared/interfaces/project';
-import { Skill } from '../shared/interfaces/skill';
 import { ProgrammingLanguageListService } from '../services/programming-language-list.service';
 
+/**
+ * Component responsible for displaying the overview of a single 
+ * project.
+ *
+ * Retrieves project details based on the route parameter
+ * and provides references to the required programming languages and 
+ * tools.
+ */
 @Component({
   selector: 'app-single-project-overview',
   imports: [CommonModule, TranslateModule, RequiredSkillsComponent],
@@ -15,22 +22,78 @@ import { ProgrammingLanguageListService } from '../services/programming-language
   styleUrl: './single-project-overview.component.scss'
 })
 export class SingleProjectOverviewComponent {
+  // #region ATTRIBUTES
+  /**
+   * Activated route instance used to retrieve the current project name
+   * from the route parameters.
+   */
   route = inject(ActivatedRoute);
+
+  /**
+   * Service providing access to the list of project descriptions.
+   */
   projectdesclist = inject(ProjectDescriptionListService);
+
+  /**
+   * The project to display, determined by the `projectname` route parameter.
+   *
+   * @type {Project | undefined}
+   */
   proj?: Project= this.projectdesclist.projDescriptionList.find(pr => pr.projname === this.route.snapshot.paramMap.get('projectname'));
+  
+  /**
+   * Service providing access to the list of programming languages.
+   */
   proglanguagelist = inject(ProgrammingLanguageListService);
   
+  /**
+   * Programming language reference for HTML, if available.
+   */
   html = this.proglanguagelist.progLanguageList.find(pl => pl.name === 'HTML');
+  
+  /**
+   * Programming language reference for CSS, if available.
+   */
   css = this.proglanguagelist.progLanguageList.find(pl => pl.name === 'CSS');
+  
+  /**
+   * Programming language reference for JavaScript, if available.
+   */
   js = this.proglanguagelist.progLanguageList.find(pl => pl.name === 'JavaScript');
+  
+  /**
+   * Framework reference for Angular, if available.
+   */
   angular = this.proglanguagelist.progLanguageList.find(pl => pl.name === 'Angular');
+  
+  /**
+   * Programming language reference for TypeScript, if available.
+   */
   ts = this.proglanguagelist.progLanguageList.find(pl => pl.name === 'TypeScript');
+  
+  /**
+   * Service/tool reference for Firebase, if available.
+   */
   firebase = this.proglanguagelist.progLanguageList.find(pl => pl.name === 'Firebase');
+  // #endregion
 
+  // #region METHODS
+  /**
+   * Lifecycle hook called on component initialization.
+   *
+   * Retrieves the current project name from the route snapshot.
+   *
+   * @returns {void}
+   */
   ngOnInit(){
     const projectname = this.route.snapshot.paramMap.get('projectname');
   }
 
+  /**
+   * Determines the width of an underline element based on the current project name.
+   *
+   * @returns {number} The underline width in pixels.
+   */
   setUnderlineWidth(): number {
     switch (this.proj?.projname) {
       case 'Join':
@@ -43,4 +106,5 @@ export class SingleProjectOverviewComponent {
         return 100; 
     }
   } 
+  // #endregion
 }
